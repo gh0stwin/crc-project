ca2bcc4d2fbf8ad30925d2ab7a0e6ec2632514b0
Fabio Vital <fabiovital@live.com.pt>
2019-11-25 15:18:16 +0000

    update SirHandler

--- proj2/sir_handler.py
+++ proj2/sir_handler.py
@@ -8,34 +8,54 @@ class SirHandler(object):
     def __init__(self):
         self._store_path = pl.Path('./simulations/')
 
-    def simulate(self, net_path, betas, fs, times_per_beta_f, seed=0):
-        g = nx.read_gml(net_path)
+    def simulate(self, files, betas, fs, times_per_beta_f, seed=0):
+        for file in files:
+            g = nx.read_gml(file)
 
-        for beta in betas:
-            for f in fs:
-                for i in range(times_per_beta_f):
-                    net_props = net_path.split('/')[-1].split('_')[0:3]
-                    print(
-                        (
-                            'network: {}, nodes: {}, index: {}, '
-                            'beta: {}, f: {}, seed: {}'.format(
-                                *net_props, beta, f, seed 
-                            )
-                        ),
-                        end='\r'
+            for beta in betas:
+                for f in fs:
+                    self._simulate_for_net_beta_f(
+                        self._create_res_filename(file, beta, f),
+                        g, 
+                        beta, 
+                        f, 
+                        times_per_beta_f, 
+                        seed
                     )
-                    res = Sir(g.copy(), beta, f, seed).simulate()
-                    self._write_in_file(res, net_path, beta, f, seed)
-                    seed += 1
+
+    def _create_res_filename(self, file, beta, f):
+        return self._store_path.joinpath(
+            (
+                pl.Path(file).stem.rsplit('_', 1)[0] + 
+                '_{}_{}'.format(beta, f).replace('.', '')
+            ), 
+            '.out'
+        )
+
+    def _simulate_for_net_beta_f(self, res_nm, g, beta, f, iters, seed):
+        res_f = open(res_nm, 'w')
+
+        for i in range(iters):
+            print('network: {}, iter: {}'.format(res_f, i), end='\r')
+            r = Sir(g.copy(), beta, f, seed).simulate()
+            res_f.write('{},{},{}\n'.format(seed, r[-1][0], r[-1][0]))
+            seed += 1
+        
+        res_f.close()
 
     def _write_in_file(self, result, net_path, beta, f, seed):
         file_path = self._store_path.joinpath(
             (
-                pl.Path(net_path).stem + 
-                '_{}_{}_{}.out'.format(beta, f, seed).replace('.', '')
+                pl.Path(net_path).stem.rsplit('_', 1)[0] + 
+                '_{}_{}'.format(beta, f).replace('.', '')
             ), 
+            '.out'
         )
 
         f = open(file_path, 'w')
-        f.write('{},{}'.format(result[-1][0], result[-1][1]))
+        f.write('{},{},{}'.format(seed, result[-1][0], result[-1][1]))
         f.close()
+
+if __name__ == '__main__':
+    sh = SirHandler()
+    sh.simulate('./networks/ba_625_1_0.gml', 0.03125, 0.1, 300000)
